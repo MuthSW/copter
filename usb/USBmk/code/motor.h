b8856164c9c289de7ee934256aa2dc42ce35dc03
Markus Knapp <github@direcs.de>
2012-10-26 23:56:59 +0200

    Removed old direcsstm files

--- usb/USBmk/code/motor.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTOR_H
-#define MOTOR_H
-
-#include "main.h"
-
-// the prototypes ------------------------------------------------------------
-int motorControl(int motor, int power, int direction);
-
-#endif
